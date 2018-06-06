7d3cfdf3aeb51a385c5692d11012f86e0e8ec97f
gianni963 <gianni_guatieri@hotmail.com>
2018-06-06 22:29:05 +0200

    ajout hmlm css

--- public/index.php
+++ public/index.php
@@ -1,6 +1,30 @@
 <?php
 
 	require_once __DIR__.'/../bootstrap/app.php';
+<<<<<<< HEAD
+?>
+ <link rel="stylesheet" type="text/css" href="/public/design.css">	
+ 	<p>Hi we are in index.php</p>
+ 	<table style="width:100%">
+		<tr>
+			<th>titre</th>
+			<th>name</th>
+			<th>last name</th>
+		</tr>
+		<tr>
+			<td>inspecteur</td>
+			<td>Gianni</td>
+			<td>Guatieri</td>
+		</tr>
+		<tr>
+			<td>Carabinier</td>
+			<td>Simone</td>
+			<td>Alfieri</td>
+		</tr>
+	</table>
+=======
 
 
 ?>
+<p>nice</p>
+>>>>>>> hotfix-html
